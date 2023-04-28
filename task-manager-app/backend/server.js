b94ea386fba4e2ec716f9facd7a00cfbf2024345
falmatam <you@example.com>
2023-04-27 23:40:23 -0700

    updated package json with auto scripts

--- task-manager-app/backend/server.js
+++ task-manager-app/backend/server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+
+
+const app = express();
+
+//const port = 8885;
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+})
